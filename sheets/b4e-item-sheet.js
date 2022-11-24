b2c790b9bb97d32eadbb7202f94a84c19e61e1bc
Ryan Anderson <anderson.ryan.w@live.com>
2022-11-24 14:42:35 -0600

    Added skill modifiers section to species sheet

--- sheets/b4e-item-sheet.js
+++ sheets/b4e-item-sheet.js
@@ -39,6 +39,9 @@ export class b4eItemSheet extends ItemSheet {
             if(formData.system['abilities']){
                 formData.system.abilities = Object.values(formData.system.abilities);
             }
+            if(formData.system['skills']){
+                formData.system.skills = Object.values(formData.system.skills);
+            }
         }
         super._updateObject(event, flattenObject(formData));
     }
@@ -46,6 +49,9 @@ export class b4eItemSheet extends ItemSheet {
     setSize() {
         if(this.item.type === "skill"){
             this.setPosition({width: 480, height: 170});
+        }
+        if(this.item.type === "species"){
+            this.setPosition({width: 800, height: 400});
         }
     }
 
@@ -75,6 +81,33 @@ export class b4eItemSheet extends ItemSheet {
             const abilities = this.item.system.abilities;
             abilities.splice(index, 1);
             this.item.system.abilities = abilities;
+            if(this.actor){
+                this.actor.prepareDerivedData();
+                this.actor.sheet.render();
+            }
+            this.render();
+        });
+
+        $('.species-sheet .add-skill').click(ev => {
+            const skills = this.item.system.skills;
+            skills.push({
+                skill: "",
+                bonus: 0
+            });
+            this.item.system.skills = skills;
+            if(this.actor){
+                this.action.prepareDerivedData();
+                this.actor.sheet.render();
+            }
+            this.render();
+        });
+
+        $('.species-sheet .delete-skill').click(ev => {
+            let idString = $(ev.currentTarget).attr("id");
+            let index = idString.split(":")[1];
+            const skills = this.item.system.skills;
+            skills.splice(index, 1);
+            this.item.system.skills = skills;
             if(this.actor){
                 this.actor.prepareDerivedData();
                 this.actor.sheet.render();
